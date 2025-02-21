e67a9dd1aab87db140194f8c24684de06efd3dda
Andrej Milanović <amilanovi0@gmail.com>
2025-02-21 12:04:58 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': 'error',
+  },
+};
---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <h1 className="text-5xl">Welcome to OllamaChat</h1>
+      </main>
+      <aside></aside>
+    </>
+  );
+}
